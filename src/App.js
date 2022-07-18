c61d08480df4895a83d90151bbe542bec1ff277d
gem <chiguayeshao@gmail.com>
2022-07-19 00:05:39 +0800

    add forecast component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CurrentWeather from './components/current-weather/current-weather';
 import Search from './components/search/search';
 import { useState } from 'react';
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import Forecast from './components/forecast/forecast';
 
 function App() {
 
@@ -33,6 +34,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
