b9db16a7a8b52d2c710638147789ec4895a564ff
gem <chiguayeshao@gmail.com>
2022-07-18 23:35:33 +0800

    add search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '3aa9052e00mshb33c0adb8470990p16829ajsn2165b08325e9',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEP_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
---
+++ src/components/current-weather/current-weater.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CurrentWeather = () => {
+    return <div>current-weater</div>
+}
+
+export default CurrentWeather
